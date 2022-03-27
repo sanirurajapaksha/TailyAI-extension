cb6845a6c4677f1cc46f05e04ae738855fd80dea
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-03-27 19:30:58 +0530

    main

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app"
 
 try {
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: 'AIzaSyDWQxDBidHNWUOIoc2C-UllBK389TUuQxw',
-    authDomain: 'test-cbdbf.firebaseapp.com',
-    projectId: 'test-cbdbf',
-    storageBucket: 'test-cbdbf.appspot.com',
-    messagingSenderId: '882038101179',
-    appId: '1:882038101179:web:9e9c2318bf8e7d6bf1459e',
+    apiKey: "AIzaSyDWQxDBidHNWUOIoc2C-UllBK389TUuQxw",
+    authDomain: "test-cbdbf.firebaseapp.com",
+    projectId: "test-cbdbf",
+    storageBucket: "test-cbdbf.appspot.com",
+    messagingSenderId: "882038101179",
+    appId: "1:882038101179:web:9e9c2318bf8e7d6bf1459e",
   }
   // Initialize Firebase
   const app = initializeApp(firebaseConfig)
-  console.log('Firebase Initialized')
+  console.log("Firebase Initialized")
 } catch (e) {
   //error
   console.log(e)
