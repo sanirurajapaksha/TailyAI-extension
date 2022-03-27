9a667dbff20a0a34a4be9566ca323f0f730bd4f5
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-03-27 19:37:23 +0530

    main

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,25 @@
-import path from 'path'
+import path from "path";
 
-import { babel } from '@rollup/plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
-import resolve from '@rollup/plugin-node-resolve'
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
 
-import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
-import { emptyDir } from 'rollup-plugin-empty-dir'
-import zip from 'rollup-plugin-zip'
+import {
+  chromeExtension,
+  simpleReloader,
+} from "rollup-plugin-chrome-extension";
+import { emptyDir } from "rollup-plugin-empty-dir";
+import zip from "rollup-plugin-zip";
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === "production";
 
 export default {
-  input: 'src/manifest.json',
+  input: "src/manifest.json",
   output: {
-    dir: 'dist',
-    format: 'esm',
-    chunkFileNames: path.join('chunks', '[name]-[hash].js'),
+    dir: "dist",
+    format: "esm",
+    chunkFileNames: path.join("chunks", "[name]-[hash].js"),
   },
   plugins: [
     chromeExtension(),
@@ -25,18 +28,18 @@ export default {
     // Replace environment variables
     replace({
       preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
     babel({
       // Do not transpile dependencies
-      ignore: ['node_modules'],
-      babelHelpers: 'bundled',
+      ignore: ["node_modules"],
+      babelHelpers: "bundled",
     }),
     resolve(),
     commonjs(),
     // Empties the output dir before a new build
     emptyDir(),
     // Outputs a zip file in ./releases
-    isProduction && zip({ dir: 'releases' }),
+    isProduction && zip({ dir: "releases" }),
   ],
-}
+};
