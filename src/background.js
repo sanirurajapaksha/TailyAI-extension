aa36a11671892906d9f9e7c9e1f2b6b112ce8aba
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2022-03-28 21:20:07 +0530

    main

---
+++ src/background.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWQxDBidHNWUOIoc2C-UllBK389TUuQxw",
+  authDomain: "test-cbdbf.firebaseapp.com",
+  projectId: "test-cbdbf",
+  storageBucket: "test-cbdbf.appspot.com",
+  messagingSenderId: "882038101179",
+  appId: "1:882038101179:web:9e9c2318bf8e7d6bf1459e",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const user = auth.currentUser;
+if (user) {
+  console.log("User logged in");
+} else {
+  console.log("User not logged in");
+}
